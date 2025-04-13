8bf70f583e203db51d5fa25a391548ef69e33e26
Ouattara Kiboyou mohamed <86033140+kiboyou@users.noreply.github.com>
2025-04-13 21:47:16 +0100

    ajout de API

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
     setMessageStatus(""); // Réinitialise le message de statut
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/contact", {
+      const response = await fetch("https://api-okmfolio.onrender.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
